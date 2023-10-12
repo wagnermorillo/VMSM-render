6cca037d386cb1aa79d5b9337cd5189d19ae432f
Wagner Morillo <1107665@est.intec.edu.do>
2023-10-12 15:59:57 -0400

    deploy5

--- project/build.sh
+++ project/build.sh
@@ -6,3 +6,5 @@ pip install --upgrade pip && pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'password')" | python manage.py shell
